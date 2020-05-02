13c0d9152c7143c3137c53133c001a6d977f623e
bellefab <bellefab@yahoo.fr>
2020-05-02 10:44:39 +0200

    Rework command line - add messages
    Use getopt to handle command line option.
    Now we can specify the output file.
    Type is for the moment always JSON.
    Opetrations details are now taken and put un JSON file.

--- extract-cc-pdf.py
+++ extract-cc-pdf.py
@@ -2,6 +2,7 @@
 
 import json
 import sys
+import getopt
 import camelot
 
 
@@ -20,20 +21,17 @@ class Mouvement:
         pass
 
     def __repr__(self):
-        return ("date=%s montant=%s" % (self.date, self.montant))
+        return ("date=%s montant=%s message=%s" % (self.date, self.montant, self.message))
         # str("date=%s montant=%s" % (self.date, self.montant))
 
 
 def serialize_objects(obj):
-
-    # serialize datetime object
-
     if isinstance(obj, Mouvement):
         return {
             'date': obj.date,
             'montant': obj.montant,
-            'credit': obj.credit # ,
-            # TODO 'message': obj.message
+            'credit': obj.credit,
+            'message': obj.message
         }
     raise TypeError(str(obj) + ' is not JSON serializable')
 
@@ -100,11 +98,13 @@ def treat_tables_ccFormat(tables):
     nb_mvnt = 0
 
     for i in range(1, tables.n-1):
-        # for all lines
         td = tables[i].df
         rows_nb = td.shape[0]
         cols_nb = td.shape[1]
         in_table = False
+        message_multLine = False
+        in_ope = False
+        # for all lines
         for row in range(rows_nb):
             if td.iat[row, 0] == "Date" and \
              ("Détail des opérations" in td.iat[row, 1]):
@@ -120,8 +120,9 @@ def treat_tables_ccFormat(tables):
             elif not in_table:
                 continue
             elif td.iat[row, 0] == "" and \
-             ("SOLDE PRECEDENT" in td.iat[row, 1]):
+              ("SOLDE PRECEDENT" in td.iat[row, 1]):
                 print("SOLDE PRECEDENT FOUND\n")
+                in_ope = False
                 if td.iat[row, credit_col] != "":
                     solde_precedent = td.iat[row, credit_col]
                 else:
@@ -130,6 +131,7 @@ def treat_tables_ccFormat(tables):
             elif td.iat[row, 0] == "" and \
              ("NOUVEAU SOLDE" in td.iat[row, 1]):
                 print("NOUVEAU SOLDE FOUND\n")
+                in_ope = False
                 if td.iat[row, credit_col] != "":
                     solde_nouveau = td.iat[row, credit_col]
                 else:
@@ -137,6 +139,7 @@ def treat_tables_ccFormat(tables):
                 # TODO
             elif td.iat[row, 0] != "":
                 # new mvnt
+                in_ope = True
                 mvnt = Mouvement()
                 mvnt.date = td.iat[row, 0]
                 if td.iat[row, credit_col] != "":
@@ -145,9 +148,19 @@ def treat_tables_ccFormat(tables):
                 else:
                     mvnt.credit = False
                     mvnt.montant = td.iat[row, debit_col]
+                # get operations details, and continue if following row has col0
+                # empty (???)
+                mvnt.message = td.iat[row, 1]
+                if row < (rows_nb-1) and td.iat[row+1, 0] == "":
+                    message_multLine = True
                 mvnt_list.append(mvnt)
                 nb_mvnt = nb_mvnt + 1
+            elif td.iat[row, 0] == "" and \
+              message_multLine and in_ope:
                 # Set message !!!
+                mvnt.message = mvnt.message + td.iat[row, 1]
+                if row < (rows_nb-1) and td.iat[row+1, 0] != "":
+                    message_multLine = False
 
     print("---------------------------------\n\n")
     print("------------nb_mvnt =%d---------------------\n\n" % (nb_mvnt))
@@ -155,12 +168,17 @@ def treat_tables_ccFormat(tables):
 
     check_soldes(solde_precedent, solde_nouveau, mvnt_list)
 
-    with open('data.json', 'w') as f:
-        solde = {}
-        solde['solde_precedent_date'] = '0'
-        solde['solde_precedent_montant'] = solde_nouveau
-        solde['solde_nouveau_date'] = '0'
-        solde['solde_nouveau_montant'] = solde_nouveau
+    solde = {}
+    solde['solde_precedent_date'] = '0'
+    solde['solde_precedent_montant'] = solde_precedent
+    solde['solde_nouveau_montant'] = solde_nouveau
+
+    return solde, mvnt_list
+
+
+def write_file(solde, mvnt_list, type, outputfile):
+    # TODO treat type other than json
+    with open(outputfile, 'w') as f:
         data_all = {}
         data_all['solde'] = solde
         data_all['operations'] = mvnt_list
@@ -170,23 +188,46 @@ def treat_tables_ccFormat(tables):
 def usage():
     print(" CLI programm to extract Credit Cooperatif Bank data from PDF.\n \
     Extraction is writen in JSON.\n \
-     Usage : $ extract-cc-pdf RELEVE.pdf\n \
-     Extract data from RELEVE.pdf and write them in RELEVE.json")
+     Usage : $ extract-cc-pdf -i  <inputfile.pdf> -t <type> -o <outputfile.json>\n \
+     Extract data from inputfile.pdf and write them in type (json) format \
+     in outputfile.json")
 
 
 if __name__ == "__main__":
-    print(len(sys.argv))
-    if len(sys.argv) != 2:
+    inputfile = ""
+    outputfile = ""
+    try:
+        opts, args = getopt.getopt(sys.argv[1:], "hi:o:t:", ["help", "ifile=", "ofile=", "type="])
+    except getopt.GetoptError:
+        print("Error in command line.")
         usage()
-    else:
-        #file = "/home/fbelle-local/Téléchargements/RELEVES_0902258281_20191101.pdf"
-        #file = "RELEVE.pdf"
-        # PDF file to extract tables from
-        file = sys.argv[1]
-        # TODO check that is a file, we can open, that is a PDF
+        sys.exit(2)
+    for opt, arg in opts:
+        print(opt)
+        print(arg)
+        if opt == "-h":
+            usage()
+            sys.exit()
+        elif opt in ("-i", "--ifile"):
+            # PDF file to extract tables from
+            #file = "/home/fbelle-local/Téléchargements/RELEVES_0902258281_20191101.pdf"
+            #file = "RELEVE.pdf"
+            inputfile = arg
+        elif opt in ("-o", "--ofile"):
+            outputfile = arg
+        elif opt in ("-t", "--type"):
+            type = "json"  # TODO
+    if args or len(opts) > 3:
+        usage()
+        sys.exit()
+    if inputfile == "" or outputfile == "":
+        usage()
+        sys.exit()
+    # TODO check that is a file, we can open, that is a PDF
 
-        # extract all the tables in the PDF file
-        tables = camelot.read_pdf(file, pages='all', flavor='stream')
+    # extract all the tables in the PDF file
+    tables = camelot.read_pdf(inputfile, pages='all', flavor='stream')
 
-        print_debug1(tables)
-        treat_tables_ccFormat(tables)
+    print_debug1(tables)
+    solde, mvnt_list = treat_tables_ccFormat(tables)
+    write_file(solde, mvnt_list, type, outputfile)
